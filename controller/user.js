241ceb3d48b2741b2bb1d1be076f15140a0468d8
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-17 03:25:02 +0900

    user controller draft

--- controller/user.js
+++ controller/user.js
@@ -1,13 +1,14 @@
-const passport = require("../passport");
+const passport = require('passport');
 // const Sequelize = require('sequelize');
 // const { User } = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 const kakaoCallback = (req, res, next) => {
-    passport.authenticate('kakao', { failureRedirect: '/' }, (err, user, info) => {
+    /*req.app.get('*/passport/*')*/.authenticate('kakao', { failureRedirect: '/' }, (err, user, info) => {
         if (err) return next(err);
         const { userId, nickname } = user;
         const token = jwt.sign({ userId: userId }, process.env.TOKENKEY);
+        console.log(token);
         result = {
             token: token,
             nickname: nickname,
@@ -19,7 +20,7 @@ const kakaoCallback = (req, res, next) => {
 };
 
 const  googleCallback = (req, res, next) => {
-    passport.authenticate('google', { failureRedirect: '/' }, (err, user, info) => {
+    /*req.app.get('*/passport/*')*/.authenticate('google', { failureRedirect: '/' }, (err, user, info) => {
         if (err) return next(err);
         const { userId, nickname } = user;
         const token = jwt.sign({ userId: userId }, process.env.TOKENKEY);
@@ -33,7 +34,7 @@ const  googleCallback = (req, res, next) => {
 };
 
 const naverCallback = (req, res, next) => {
-    passport.authenticate('naver', { failureRedirect: '/' }, (err, user, info) => {
+    /*req.app.get('*/passport/*')*/.authenticate('naver', { failureRedirect: '/' }, (err, user, info) => {
         if (err) return next(err);
         const { userId, nickname } = user;
         const token = jwt.sign({ userId: userId }, process.env.TOKENKEY);
