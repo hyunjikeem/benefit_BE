7e94ff128a40a996f27794cc9a95a26939ca79e9
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-17 03:27:24 +0900

    app.js draft

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ require('dotenv').config(); // 환경변수
 const express = require('express');
 const { sequelize } = require('./models');
 const app = express();
+// const passport = require('passport');
 const passportConfig = require('./passport');
 const port = 3000;
 const cors = require('cors');
 // const router = require('./routes/auth');
 
 app.use(cors());
-passportConfig();
+passportConfig(app);
 
 // force: 서버 실행 시 마다 테이블을 재생성 할 것인지 아닌지
 sequelize.sync({ force: false }).then(() => {
@@ -18,8 +19,10 @@ sequelize.sync({ force: false }).then(() => {
 });
 
 const userRouter = require('./routes/user');
+const category1Router = require('./routes/category1');
 
-app.use('/api', [userRouter]);
+app.use('/auth', [userRouter]);
+app.use([category1Router]) 
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
