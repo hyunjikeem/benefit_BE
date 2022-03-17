2e686d8b18a67eb4533e625ea993a1ab979fd5c7
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-17 21:34:45 +0900

    category routes 0317

--- routes/category1.js
+++ routes/category1.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getPolicy } = require('../controller/category1')
 
-router.get('/search/:params', getPolicy)
+router.post('/search', getPolicy)
 
 module.exports = router;
