9d18d03c142e078f22fc365e472b2df9ab65fa8e
hyunjikeem <91252981+hyunjikeem@users.noreply.github.com>
2022-03-18 05:12:21 +0900

    Update user.js
    0318.5

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 // const Sequelize = require('sequelize');
-// const { User } = require('../models/user');
+const { User } = require('../models');
 const jwt = require('jsonwebtoken');
 
 const kakaoCallback = (req, res, next) => {
