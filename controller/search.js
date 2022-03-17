730b189bf5c19cab29673b370fdf16334dc5bd4f
Ausdauer1 <steady051@naver.com>
2022-03-17 21:40:20 +0900

    3.17.2

--- controller/search.js
+++ controller/search.js
@@ -6,21 +6,36 @@ const { fn, col } = Policy.sequelize;
 
 exports.searchResults = async (req, res) => {
     try {
-        const filter = req.body;
-        const location = filter.location
-
-      let searchWords = [];
-      console.log(searchWords)
+      const {location, benefit} = req.body;
+        // const location = filter.location
+      let locationWords = [];
+      
       for (let i of location) {
-        searchWords.push({ location: { [Op.like]: `%${i}%` } })
+          if ( i === 'all') {
+            locationWords.push({ location: { [Op.like]: '%%' } })
+          } else {
+            locationWords.push({ location: { [Op.like]: `%${i}%` } })
+          }
       }
-      console.log(searchWords)
+      
+      let educationWord;
+      if (benefit === 'all') {
+          educationWord = { benefit: { [Op.like]: '%%' } } 
+      } else {
+          educationWord = { benefit: { [Op.like]: `%${benefit}%` } }
+      }
       
       const c0 = await Policy.findAll({
           attributes:['postId', 'category', 'benefit', 'title', [fn('concat', col('apply_start'), ' ~ ', col('apply_end')), "apply_period"], 'view', 'operation','location'],
-          where: { [Op.or]: searchWords
-          }
+          where: {
+              [Op.and] : [
+                  {category : "주거·금융"},
+                  { [Op.or]: locationWords },
+                   educationWord,
+            ]
+          }, 
       });
+      
 
 
       res.json({ c0 });
