ee8bab96873b388e5eb0ad8fa68f1b53383931a6
HyunjiKim818 <kjhxxxx@gmail.com>
2022-03-24 07:49:59 +0900

    Add main routes by jh 0324-1

---
+++ routes/main.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const freeMiddleware = require('../middleware/free-middleware');
+
+const controller = require("../controller/main");
+
+// 메인페이지 
+router.get("/main", controller.mainpage);
+
+// 상세페이지 
+router.get("/detail/:postId", freeMiddleware, controller.detailpage);
+
+
+module.exports = router;
