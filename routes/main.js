a45ef35f1cfa52318713a596f2420130966baa44
Ausdauer1 <97427803+Ausdauer1@users.noreply.github.com>
2022-03-24 00:46:35 +0900

    update 3.24.2

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const freeMiddleware = require('../middleware/free-middleware');
 
-// const authMiddleware = require("../middlewares/auth-middleware");
 const controller = require("../controller/main");
 
-// 메인페이지 신상품
+// 메인페이지 
 router.get("/main", controller.mainpage);
 
-// 서브페이지 베스트
-router.get("/detail/:postId", controller.detailpage);
+// 상세페이지 
+router.get("/detail/:postId", freeMiddleware, controller.detailpage);
 
 
 module.exports = router;
