c01f9aada4cb79d0f9675d16635d5149c5426916
HyunjiKim818 <kjhxxxx@gmail.com>
2022-04-05 13:24:30 +0900

    Deleted console.log in user controller 0405.2

--- controller/user.js
+++ controller/user.js
@@ -11,13 +11,12 @@ const kakaoCallback = (req, res, next) => {
         if (err) return next(err);
         const { userId, nickname } = user;
         const token = jwt.sign({ userId: userId }, process.env.TOKENKEY);
-        // console.log(token);
         result = {
             token: token,
             nickname: nickname,
             userId,
         };
-        
+
         const find_folder = await Zzim_folder.findOne({
             where: { userId }
         });
@@ -35,8 +34,7 @@ const kakaoCallback = (req, res, next) => {
         }
 
         const result2 = await sequelize.query(`SELECT f.folder_name, f.folderId, f.folder_status, group_concat(z.postId) as postId_list FROM Zzim_folders as f LEFT JOIN Zzims as z on z.folderId = f.folderId where f.userId = ${userId} group by f.folderId;`, { type: QueryTypes.SELECT });
-        // console.log(result);
-        // console.log(result2);
+        
         res.send({ user: result, result2 });
     })(req, res, next);
 };
